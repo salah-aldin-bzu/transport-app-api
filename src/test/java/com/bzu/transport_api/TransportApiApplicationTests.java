15beeffc16c122f4d8f6f529336f9c9cb05a57a0
Salah-Aldin Amera <60547056+salahaldinamera@users.noreply.github.com>
2022-03-06 20:17:57 +0200

    first commit

---
+++ src/test/java/com/bzu/transport_api/TransportApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bzu.transport_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
